d8346e23b5d4933e137f778e53289d2682809ef6
Irina Balkarova <irb023@post.uit.no>
2018-12-03 19:55:57 +0100

    Homework10 v1.0

---
+++ lesson10/homework/script.js
@@ -0,0 +1,23 @@
+class Options {
+  constructor(height, width, bg, fontSize, textAlign) {
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+    this.textAlign = textAlign;
+  }
+
+  newDiv() {
+    let newDiv = document.createElement("DIV");
+    newDiv.style.cssText = 
+    `height:${this.height}px;width:${this.width}px;background-color:${this.bg};font-size:${this.fontSize}px;text-align:${this.textAlign};`;
+
+    return newDiv;
+  }
+}
+
+let myEl = new Options(30, 30, 'yellow', 30,'left');
+
+console.log(myEl.newDiv());
+
+document.newDiv().innerHTML = "Hello!";

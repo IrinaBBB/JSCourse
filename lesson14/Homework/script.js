d9bc141fb272e28a312adceb26e4ad50fa697411
Irina Balkarova <irb023@post.uit.no>
2018-12-10 14:03:46 +0100

    lesson14

--- lesson14/Homework/script.js
+++ lesson14/Homework/script.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
-    $('.main_btna text-center').on('click', function () {
+    $('.main_btna').on('click', function () {
         $('.overlay').fadeIn('slow');
+        $('.modal').slideDown('slow');
+        
+    });
+
+    $('.main_btn').on('click', function () {
+        $('.overlay').fadeIn('slow');
+        $('.modal').slideDown('slow');
+    });
+
+    $('.close').on('click', function() {
+        $('.overlay').fadeOut('slow');
+        $('.modal').slideUp('slow');
     });
 
 });
